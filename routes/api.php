331cc4fb28a3a7ec7a77c7c5292f4eb6a5135469
adxmdd <yamincode@gmail.com>
2023-01-31 19:09:49 +0700

    API Register

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\ProductCategoryController;
+use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,3 +22,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('products', [ProductController::class, 'all']);
 Route::get('categories', [ProductCategoryController::class, 'all']);
+Route::get('register', [UserController::class, 'register']);
